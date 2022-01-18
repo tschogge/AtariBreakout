eb10d4e399b4d3340c09697e6abb1e3e806e3398
Giotsche <gioele.petrillo@gmail.com>
2022-01-18 09:01:54 +0100

    start

---
+++ core.js
@@ -0,0 +1,9 @@
+function drawGame(canvas) {
+    let ctx = canvas.getContext("2d");
+}
+
+window.onload = () => {
+    /**@type{HTMLCanvasElement}*/
+    let canvas = document.getElementById("theGame");
+    drawGame(canvas);
+}
