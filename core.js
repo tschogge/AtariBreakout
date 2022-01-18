456911babdb2957cd2e8f20622f091c2ce159ef8
Giotsche <gioele.petrillo@gmail.com>
2022-01-18 09:34:56 +0100

    start

--- core.js
+++ core.js
@@ -1,9 +1,19 @@
-function drawGame(canvas) {
+function startSpiel() {
+    //Der start knopf wird verborgen
+    document.getElementById("startSpiel").style.display = "none";
+
+    //Canvas wird erstellt
+    /** @type{HTMLCanvasElement}*/
+    let canvas = document.getElementById("theGame");
     let ctx = canvas.getContext("2d");
+
+    //Das canvas wird angezeigt
+    canvas.style.visibility = "visible";
+
+    rechteckZeichnen(ctx, 50, 10);
 }
 
-window.onload = () => {
-    /**@type{HTMLCanvasElement}*/
-    let canvas = document.getElementById("theGame");
-    drawGame(canvas);
+function rechteckZeichnen(ctx, x, y) {
+    ctx.fillStyle = "#c71515";
+    ctx.fillRect(x, y, 70, 30);
 }
